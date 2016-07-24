b5bc568b9e7e729133288bd139efa1cce70a67e0
zeticous <2-3-0-8i>
2016-07-24 23:34:50 +0800

    Added splash.

--- app/src/main/java/com/orbital/thegame/spiffitnesspetsimulator/launcherActivity.java
+++ app/src/main/java/com/orbital/thegame/spiffitnesspetsimulator/launcherActivity.java
@@ -1,12 +1,15 @@
 package com.orbital.thegame.spiffitnesspetsimulator;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class launcherActivity extends AppCompatActivity {
+    boolean isLoading = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +24,17 @@ public class launcherActivity extends AppCompatActivity {
                 Log.e("FirstLaunch", "UserSpirit successfully created");
                 saveSpirits();
             }
+
             Intent intent = new Intent(launcherActivity.this, firstLaunchActivity.class);
             startActivity(intent);
             finish();
+        }
 
-        } else {
+        else {
+            loadSpirits();
+            while(isLoading){
+                //BLANK
+            }
             Intent intent = new Intent(launcherActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
@@ -45,5 +54,53 @@ public class launcherActivity extends AppCompatActivity {
 
         editor.apply();
         Log.d(PTAG, "Shared Preference saved");
+    }
+
+    public void loadSpirits(){
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        int restoredRegister = prefs.getInt("register", -99999);
+        int restoredStepCount = prefs.getInt("stepCount", -99999);
+        int restoredAffinityLevel = prefs.getInt("affinityLevel", -99);
+        long restoredStartTime = prefs.getLong("startTime", -9999);
+        long restoredEndTime = prefs.getLong("endTime", -9999);
+
+        Log.d(PTAG, "" + restoredRegister);
+        Log.d(PTAG, "" + restoredStepCount);
+        Log.d(PTAG, "" + restoredStartTime);
+        Log.d(PTAG, "" + restoredEndTime);
+
+        switch(restoredRegister){
+            case Spirits.EGG_REG:
+                GameService.UserSpirit = new Egg(restoredStepCount, restoredStartTime, restoredEndTime, restoredAffinityLevel);
+                Log.d(PTAG, "EGG load successful");
+                break;
+            case Spirits.PIG_BABY_REG:
+                GameService.UserSpirit = new Pig_Baby(restoredStepCount, restoredStartTime, restoredEndTime, restoredAffinityLevel);
+                Log.d(PTAG, "PIG_BABY load successful");
+                break;
+            case Spirits.PENGUIN_BABY_REG:
+                GameService.UserSpirit = new Penguin_Baby(restoredStepCount, restoredStartTime, restoredEndTime, restoredAffinityLevel);
+                Log.d(PTAG, "PENGUIN_BABY load successful");
+                break;
+            case Spirits.PANDA_BABY_REG:
+                GameService.UserSpirit = new Panda_Baby(restoredStepCount, restoredStartTime, restoredEndTime, restoredAffinityLevel);
+                Log.d(PTAG, "PANDA_BABY load successful");
+                break;
+            case Spirits.PIG_ADULT_REG:
+                GameService.UserSpirit = new Pig_Adult(restoredStepCount, restoredStartTime, restoredEndTime, restoredAffinityLevel);
+                Log.d(PTAG, "PIG_ADULT load successful");
+                break;
+            case Spirits.PENGUIN_ADULT_REG:
+                GameService.UserSpirit = new Penguin_Adult(restoredStepCount, restoredStartTime, restoredEndTime, restoredAffinityLevel);
+                Log.d(PTAG, "PENGUIN_ADULT load successful");
+                break;
+            case Spirits.PANDA_ADULT_REG:
+                GameService.UserSpirit = new Panda_Adult(restoredStepCount, restoredStartTime, restoredEndTime, restoredAffinityLevel);
+                Log.d(PTAG, "PANDA_ADULT load successful");
+                break;
+            default:
+                Log.d(PTAG, "No spirit detected");
+        }
+        isLoading = false;
     }
 }
